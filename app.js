5c8fb15a6374c6ebbd01a749131ef00c7fd63879
YohanBZ <barbozayohan@gmail.com>
2023-08-29 23:04:23 -0300

    feat: :art: criação  lógica da ferramenta

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-import readline from "readline";
+import * as readline from 'node:readline/promises';
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-rl.question('qual seu nome?', nome => { console.log(`Praze ${nome}`)})
+
+async function lista(){ 
+    const array = []
+    let condicao = true
+    while (condicao) {
+      let item = await rl.question("Digite uma propriedade de css ou digite 'sair' para encerrar: ")
+      if(item.toLocaleLowerCase() != 'sair'){
+        array.push(item)
+      }else{
+        condicao = false
+      }
+    }
+    array.sort().forEach((element) => {
+      console.log(`- ${element}`)
+    });
+  }
+  
+  lista()
