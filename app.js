235394dbf993739d17592d3ebb030bcb87c5ad8c
YohanBZ <barbozayohan@gmail.com>
2023-08-29 21:19:43 -0300

    feat: :art: importação do readline e teste

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("teste")
+import readline from "readline";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('qual seu nome?', nome => { console.log(`Praze ${nome}`)})
