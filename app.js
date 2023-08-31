c8ab3f3e0cd2795162086e1a614d1d64f8f28422
YohanBZ <barbozayohan@gmail.com>
2023-08-30 22:08:22 -0300

    feat: :zap: atualização arquivo responsavel por rodar os scripts

--- app.js
+++ app.js
@@ -1,32 +1,9 @@
-import * as readline from 'node:readline/promises';
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-async function addLista(array){
-  let condicao = true
-  while (condicao) {
-    let item = await rl.question("Digite uma propriedade de css ou digite 'sair' para encerrar: ")
-    if(item.toLocaleLowerCase() != 'sair'){
-      array.push(item)
-    }else{
-      condicao = false
-    }
-  }
-  ordernar(array)
-}
+import addLista from "./src/addItemLista.js"
+import listaOrdernada from "./src/imprimir.js"
 
 async function lista(){ 
-    const array = []
-    addLista(array)
+    const CriarLista = await addLista();
+    listaOrdernada(CriarLista)
   }
 
-async function ordernar(array){
-  array.sort().forEach((element) => {
-    console.log(`- ${element}`)
-  });
-}
-
-lista()
+lista();
