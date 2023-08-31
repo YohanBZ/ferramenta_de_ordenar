c305b1ad0c8d59665d3411b49add07513b2a5442
YohanBZ <barbozayohan@gmail.com>
2023-08-31 16:02:33 -0300

    feat: :sparkles: Criação do arquivo de validação e sua utilização no projeto

--- src/addItemLista.js
+++ src/addItemLista.js
@@ -1,4 +1,6 @@
 import * as readline from 'node:readline/promises';
+import Validacao from './validacao.js';
+
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,7 +13,12 @@ async function adicionandoItemLista(){
   while (condicao) {
     let item = await rl.question("Digite uma propriedade de css ou digite 'sair' para encerrar: ")
     if(item.toLocaleLowerCase() != 'sair'){
-      array.push(item)
+      if(Validacao.validacao(item) && Validacao.existe(item, array)){
+        array.push(item)
+        console.log("✅Propriedade adicionada✅\n")
+      } else {
+        console.log("\n Valor inválido ou já existente❗️")
+      }
     }else{
       condicao = false
     }
