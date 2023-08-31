3165e0ef2f07a3f73adae14c7c854df49aafb0ae
YohanBZ <barbozayohan@gmail.com>
2023-08-30 21:35:12 -0300

    feat: :sparkles: Criação lógica do projeto

--- app.js
+++ app.js
@@ -5,21 +5,28 @@ const rl = readline.createInterface({
   output: process.stdout
 })
 
+async function addLista(array){
+  let condicao = true
+  while (condicao) {
+    let item = await rl.question("Digite uma propriedade de css ou digite 'sair' para encerrar: ")
+    if(item.toLocaleLowerCase() != 'sair'){
+      array.push(item)
+    }else{
+      condicao = false
+    }
+  }
+  ordernar(array)
+}
 
 async function lista(){ 
     const array = []
-    let condicao = true
-    while (condicao) {
-      let item = await rl.question("Digite uma propriedade de css ou digite 'sair' para encerrar: ")
-      if(item.toLocaleLowerCase() != 'sair'){
-        array.push(item)
-      }else{
-        condicao = false
-      }
-    }
-    array.sort().forEach((element) => {
-      console.log(`- ${element}`)
-    });
+    addLista(array)
   }
-  
-  lista()
+
+async function ordernar(array){
+  array.sort().forEach((element) => {
+    console.log(`- ${element}`)
+  });
+}
+
+lista()
